9d2fcb23a60f9612f2e15fd9ddaccfafd65fd219
Natary07 <71980983+Natary07@users.noreply.github.com>
2020-11-22 11:43:23 -0600

    Update sketch.js

--- p5.play-boilerplate-master/sketch.js
+++ p5.play-boilerplate-master/sketch.js
@@ -5,13 +5,15 @@ var speed, weight;
 
 
 function setup() {
-  createCanvas(800,400);
-  car=createSprite(50, 200, 50, 50);
-  car.velocityX=speed;
-  car.shapeColor="black";
-  wall=createSprite(1500,200,60, height/2);
+  createCanvas(1600,400);
   speed=random(55,90);
   weight=random(400,1500)
+  car=createSprite(50, 200, 50, 50);
+  car.velocityX=speed;
+  //car.shapeColor="black";
+  wall=createSprite(1500,200,60, height/2);
+  wall.shpaeColor=color(80,80,80);
+  
 
   
 
@@ -26,14 +28,14 @@ function draw() {
     var deformation=0.5*weight*speed*speed/22500;
     if(deformation>180)
     {
-      car.shapeColor="red"
+      car.shapeColor=color(0, 255, 0)
     }
     if(deformation<180 && deformation>100)
     {
-      car.shapeColor="yellow"
+      car.shapeColor=color(230,230,0)
     }
     if(deformation<100)
-    {car.shpaeColor="green"}
+    {car.shpaeColor=color(0,255,0)}
 
 
 
